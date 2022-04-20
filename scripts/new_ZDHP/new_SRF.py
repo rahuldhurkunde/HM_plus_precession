cd4ab0183ad3b1193501de99e70fdad54ed6ba67
Rahul Dhurkunde <rahul.dhurkunde@condor5.atlas.local>
2022-04-20 12:16:58 +0000

    Added new_SRF.py

--- scripts/new_ZDHP/new_SRF.py
+++ scripts/new_ZDHP/new_SRF.py
@@ -14,12 +14,29 @@ from matplotlib.colors import BoundaryNorm
 from matplotlib.ticker import MaxNLocator
 import pandas as pd
 import argparse as ap
+from matplotlib import colors
 from matplotlib.patches import Rectangle
 import os
 lal.ClobberDebugLevel(0)
 
+class MidpointNormalize(colors.Normalize):
+    """
+    Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
+
+    e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
+    """
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
+
 def read_FFs(inj_type):
-	filename = "%s/10000_FFs.hdf" %(inj_type)
+	filename = "%s/50000_FFs.hdf" %(inj_type)
 	f = h5py.File(filename, 'r')
 
 	FF = np.array(f['FF'])
@@ -27,18 +44,6 @@ def read_FFs(inj_type):
 	m2 = np.array(f['mass2'])
 	sigmasqs = np.array(f['sigmasqs'])
 	return FF, sigmasqs, m1, m2
-
-def read_SRF(nbinsx, nbinsy, inj_type, nsets):
-	SRF = [] 
-	for row in range(2):
-		for col in range(2):
-			current_inj = inj_type[row*2 + col]
-			srf_file = "SRF_results/%sx%s/%s_%s.hdf" %(nbinsx, nbinsy, current_inj, nsets)
-			hf = h5py.File(srf_file)
-			SRF.append(hf['SRF'])
-		xedge = hf['xedge']
-		yedge = hf['yedge']
-	return xedge, yedge, np.array(SRF)
 
 def get_injInside(binnumber, bin_x, bin_y):
 	inj_with_row = np.where(binnumber[0] == bin_x+1) 
@@ -78,7 +83,7 @@ def compute_SRF(param_x, param_y, nbinsx, nbinsy, FF, sigmasqs):
 				temp_SRF = np.nan
 				print('No injections found in bin', x, y)
 
-			print([x,y], 'Inj inside', len(injInd), 'SRF', temp_SRF)
+			#print([x,y], 'Inj inside', len(injInd), 'SRF', temp_SRF)
 			indices.append(injInd)
 			SRF.append(temp_SRF)
 	return np.array(SRF), indices, xedge, yedge
@@ -99,41 +104,46 @@ def plot_SRF(SRF, xedge, yedge, fig, ax, vmax, vmin):
 	mtotal = xx + yy
 	q = np.divide(xx, yy)
 	z = SRF.reshape(len(y_new), len(x_new))
-	im = ax.pcolormesh(xx, yy, z, cmap='YlGnBu', norm=norm, shading='auto')	
+	#im = ax.pcolormesh(xx, yy, z, cmap=cmap, shading='auto', norm = MidpointNormalize(midpoint=1.0,vmin=vmin, vmax=vmax))
+	im = ax.pcolormesh(xx, yy, z, cmap=cmap, vmax=vmax, vmin=vmin, shading='auto') 
 	return im
 
 nbinsx = 10 
 nbinsy = 8 
-fig, axs = plt.subplots(1,1)
 inj_types = ['aligned', 'onlyHM', 'onlyPrecession', 'HM_prec']
-fig, ax = plt.subplots(1,1)
 
 #Command line parser
 parser = ap.ArgumentParser()
-parser.add_argument('--compute_SRF',
+parser.add_argument('--all',
 			type=int, default = 0,
 			help = 'Specify 1 to compute SRF (default option will only plot the SRF)')
 parser.add_argument('--inj_class',
-			type = int, required=True,
+			type = int,
 			help = 'Injection class (integer) 0-aligned, 1-onlyHM, 2-onlyPrecession, 3-HM_prec')
 	
 args, remaining_args = parser.parse_known_args()
 
-if args.compute_SRF == 1:
+if args.all == 1:
+	fig, axs = plt.subplots(2,2)
+
 	print("Computing SRF for bins", nbinsx, 'x', nbinsy)
-	current_inj = inj_types[args.inj_class]
-	print(current_inj)
-	
-	start = time.time()
-	FF, sigmasqs, sg_m1, sg_m2 = read_FFs(current_inj)
-	SRF, indices, xedge, yedge = compute_SRF(sg_m1, sg_m2, nbinsx, nbinsy, FF, sigmasqs)
-	end = time.time()
-	print("Time taken for SRF computation", end-start)
+	for row in range(2): 
+		for col in range(2):
+			current_inj = inj_types[row*2 + col]
+			print(current_inj)
+			ax = axs[row, col]	
 
-	vmax = np.nanmax(SRF)
-	vmin = np.nanmin(SRF)
-	#vmin = 0.1
-	print('Vmax', vmax, 'Vmin', vmin)
+			start = time.time()
+			FF, sigmasqs, sg_m1, sg_m2 = read_FFs(current_inj)
+			SRF, indices, xedge, yedge = compute_SRF(sg_m1, sg_m2, nbinsx, nbinsy, FF, sigmasqs)
+			end = time.time()
+			print("Time taken for SRF computation", end-start)
 
-	plot_SRF(SRF, xedge, yedge, fig, ax, vmax, vmin)
+			vmax = np.nanmax(SRF)
+			vmin = np.nanmin(SRF)
+			#vmin = 0.1
+			print('Vmax', vmax, 'Vmin', vmin)
+
+			plot_SRF(SRF, xedge, yedge, fig, ax, 1.0, 0.6)
+			ax.set_title(current_inj)
 	plt.show()
